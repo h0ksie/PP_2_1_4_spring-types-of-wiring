3b11abb2ddf8a877f25dfcae1e8c15f4a83541ea
Anton Obukhov <hoksiegod@gmail.com>
2023-04-27 00:22:45 +0300

    wiring

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -2,8 +2,11 @@ package koschei.models;
 
 public class Needle7 {
 
+    public Needle7() {
+    }
+
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
